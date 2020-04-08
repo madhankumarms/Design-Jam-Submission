cb4de1973f632853b97f038d0d0532dcf290218f
M S Madhan Kumar <msmadhankumar123@gmail.com>
2020-04-08 11:59:37 +0530

    Added navbar

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Smooth Scrolling Effect
+$('#navbar a').on('click', function(event){
+    if(this.hash !== ''){
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 90
+            },
+            800
+        );
+    }
+});
